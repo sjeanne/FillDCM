5261d6ee90f0265baa07485de11eaf102fed4a93
loktor <loktor@gmail.com>
2024-03-14 10:34:49 +0100

    Extract VR generators function into a separated file

---
+++ filldcm/vr_generators.py
@@ -0,0 +1,311 @@
+""" VR Generators: generate random data according to DICOM VR
+"""
+
+import string
+from random import randrange, choices
+
+PERSONAL_NAME_SAMPLE = {
+    "first_names_male": ["James",
+                         "Robert",
+                         "John",
+                         "Michael",
+                         "David",
+                         "William",
+                         "Richard",
+                         "Joseph",
+                         "Thomas",
+                         "Charles",
+                         "Christopher",
+                         "Daniel",
+                         "Matthew",
+                         "Anthony",
+                         "Mark",
+                         "Donald",
+                         "Steven",
+                         "Paul",
+                         "Andrew",
+                         "Joshua",
+                         "Kenneth",
+                         "Kevin",
+                         "Brian",
+                         "George",
+                         "Timothy",
+                         "Ronald",
+                         "Edward",
+                         "Jason",
+                         "Jeffrey",
+                         "Ryan",
+                         "Jacob",
+                         "Gary",
+                         "Nicholas",
+                         "Eric",
+                         "Jonathan",
+                         "Stephen",
+                         "Larry",
+                         "Justin",
+                         "Scott",
+                         "Brandon",
+                         "Benjamin",
+                         "Samuel",
+                         "Gregory",
+                         "Alexander",
+                         "Frank",
+                         "Patrick",
+                         "Raymond",
+                         "Jack",
+                         "Dennis",
+                         "Jerry",
+                         "Tyler",
+                         "Aaron",
+                         "Jose",
+                         "Adam",
+                         "Nathan",
+                         "Henry"],
+    "first_names_female": ["Mary",
+                           "Patricia",
+                           "Jennifer",
+                           "Linda",
+                           "Elizabeth",
+                           "Barbara",
+                           "Susan",
+                           "Jessica",
+                           "Sarah",
+                           "Karen",
+                           "Lisa",
+                           "Nancy",
+                           "Betty",
+                           "Margaret",
+                           "Sandra",
+                           "Ashley",
+                           "Kimberly",
+                           "Emily",
+                           "Donna",
+                           "Michelle",
+                           "Carol",
+                           "Amanda",
+                           "Dorothy",
+                           "Melissa",
+                           "Deborah",
+                           "Stephanie",
+                           "Rebecca",
+                           "Sharon",
+                           "Laura",
+                           "Cynthia",
+                           "Kathleen",
+                           "Amy",
+                           "Angela",
+                           "Shirley",
+                           "Anna",
+                           "Brenda",
+                           "Pamela",
+                           "Emma",
+                           "Nicole",
+                           "Helen",
+                           "Samantha",
+                           "Katherine",
+                           "Christine",
+                           "Debra",
+                           "Rachel",
+                           "Carolyn",
+                           "Janet",
+                           "Catherine",
+                           "Maria",
+                           "Heather",
+                           "Diane",
+                           "Ruth",
+                           "Julie",
+                           "Olivia",
+                           "Joyce",
+                           "Virginia"],
+    "last_names": ["Smith",
+                   "Johnson",
+                   "Williams",
+                   "Brown",
+                   "Jones",
+                   "Garcia",
+                   "Miller",
+                   "Davis",
+                   "Rodriguez",
+                   "Martinez",
+                   "Hernandez",
+                   "Lopez",
+                   "Gonzalez",
+                   "Wilson",
+                   "Anderson",
+                   "Thomas",
+                   "Taylor",
+                   "Moore",
+                   "Jackson",
+                   "Martin",
+                   "Lee",
+                   "Perez",
+                   "Thompson",
+                   "White",
+                   "Harris",
+                   "Sanchez",
+                   "Clark",
+                   "Ramirez",
+                   "Lewis",
+                   "Robinson",
+                   "Walker",
+                   "Young",
+                   "Allen",
+                   "King",
+                   "Wright",
+                   "Scott",
+                   "Torres",
+                   "Nguyen",
+                   "Hill",
+                   "Flores",
+                   "Green",
+                   "Adams",
+                   "Nelson",
+                   "Baker",
+                   "Hall",
+                   "Rivera",
+                   "Campbell",
+                   "Mitchell",
+                   "Carter",
+                   "Roberts"]
+}
+
+
+def generate_id() -> str:
+    """ Generate a Patient ID following DICOM LO VR spec
+    https://dicom.nema.org/dicom/2013/output/chtml/part05/sect_6.2.html
+    Patient ID is generated as: XXYYYY where X is a
+    Returns:
+        A Patient ID as a string
+    """
+    return "".join(choices(string.ascii_uppercase + string.digits, k=10))
+
+
+def generate_age_string() -> str:
+    """Generate an Age String and follows DICOM AS VR spec.
+    https://dicom.nema.org/dicom/2013/output/chtml/part05/sect_6.2.html
+    Only ages in years are generated: "XXXY" where 'X' are digits characters
+
+    Returns:
+        A DICOM Age String
+    """
+    return "".join(choices(string.digits, k=3)+["Y"])
+
+
+def generate_decimal_string() -> str:
+    """Generate a Decimal String and follows DICOM DS VR spec.
+    https://dicom.nema.org/dicom/2013/output/chtml/part05/sect_6.2.html
+    Only fixed point numbers are generating: only digits from 1 to 16 bytes
+
+    Returns:
+        A DICOM Decimal String
+    """
+    return "".join(choices(string.digits, k=randrange(1, 16)))
+
+
+def generate_date_time() -> str:
+    """Generate a Date Time and follows DICOM DT VR spec.
+    https://dicom.nema.org/dicom/2013/output/chtml/part05/sect_6.2.html
+    Only the following fields from DT are filled: YYYYMMDDHHMMSS
+
+    Returns:
+        A DICOM Date Time
+    """
+    return f"{generate_date()}{randrange(0, 23):02}{randrange(0, 59):02}{randrange(0, 59):02}"
+
+
+def generate_integer_string() -> str:
+    """ Generate a Integer String and follows DICOM IS VR spec.
+    https://dicom.nema.org/dicom/2013/output/chtml/part05/sect_6.2.html
+    Generate Integer in the range -2^31 <= n <= 2^31-1
+
+    Returns:
+        str: Integer string
+    """
+    return f"{randrange(-1*2**31, (2**31)-1)}"
+
+
+def generate_personal_name() -> str:
+    """ Generate a personal name and follow DICOM PN VR spec.
+    https://dicom.nema.org/dicom/2013/output/chtml/part05/sect_6.2.html
+    Only first and last names are filled.
+
+    Returns:
+        A DICOM personal name
+    """
+    possible_first_names = []
+    possible_first_names.extend(PERSONAL_NAME_SAMPLE["first_names_female"])
+    possible_first_names.extend(PERSONAL_NAME_SAMPLE["first_names_male"])
+    return f"{choices(PERSONAL_NAME_SAMPLE['last_names'])[0]}^{choices(possible_first_names)[0]}"
+
+
+def generate_date():
+    """ Generate a data and follow DICOM DA VR specs.
+    https://dicom.nema.org/dicom/2013/output/chtml/part05/sect_6.2.html
+    Years are in range [1950, 2020]
+        Returns:
+            A DICOM date
+        """
+    return f"{randrange(1950, 2020)}{randrange(1, 12):02}{randrange(1, 30):02}"
+
+
+def generate_lo():
+    """ Generate a data and follow DICOM LO VR specs.
+    https://dicom.nema.org/dicom/2013/output/chtml/part05/sect_6.2.html
+        Returns:
+            A randomized LO value with at least one character and max 64
+    """
+    return "".join(choices(string.ascii_letters + string.digits, k=randrange(1, 64)))
+
+
+def generate_long_text():
+    """ Generate a data and follow DICOM LT VR specs.
+    https://dicom.nema.org/dicom/2013/output/chtml/part05/sect_6.2.html
+        Returns:
+            A randomized LT value with at least one character and max 1024
+    """
+    return "".join(choices(string.ascii_letters + string.digits, k=randrange(1, 1024)))
+
+
+def generate_short_string():
+    """ Generate a data and follow DICOM SH VR specs.
+    https://dicom.nema.org/dicom/2013/output/chtml/part05/sect_6.2.html
+        Returns:
+            A randomized SH value with at least one character and max 16
+    """
+    return "".join(choices(string.ascii_letters + string.digits, k=randrange(1, 16)))
+
+
+def generate_short_text():
+    """ Generate a data and follow DICOM ST VR specs.
+    https://dicom.nema.org/dicom/2013/output/chtml/part05/sect_6.2.html
+        Returns:
+            A randomized ST value with at least one character and max 1024
+    """
+    return "".join(choices(string.ascii_letters + string.digits, k=randrange(1, 1024)))
+
+
+def generate_time():
+    """ Generate a data and follow DICOM TM VR specs.
+    https://dicom.nema.org/dicom/2013/output/chtml/part05/sect_6.2.html
+        Returns:
+            A randomized TM value with the format "HHMMSS"
+    """
+    return f"{randrange(0, 23):02}{randrange(0, 59):02}{randrange(0, 59):02}"
+
+
+def generate_unique_identifier():
+    """ Generate a data and follow DICOM UI VR specs.
+    https://dicom.nema.org/dicom/2013/output/chtml/part05/sect_6.2.html
+        Returns:
+            A randomized UI value with the format <digits>.<digits>.<digits>.<digits>
+    """
+    return f"{randrange(1,1000)}.{randrange(1,1000)}.{randrange(1,1000)}.{randrange(1,1000)}"
+
+
+def generate_unsigned_short():
+    """ Generate a data and follow DICOM US VR specs.
+    https://dicom.nema.org/dicom/2013/output/chtml/part05/sect_6.2.html
+        Returns:
+            A randomized US, a number in the range 0 <= x < 65536
+    """
+    return randrange(0, 65536)
---
+++ test/test_vr_generators.py
@@ -0,0 +1,141 @@
+""" DVR Generators unit tests
+"""
+
+import unittest
+import string
+from filldcm import fill_dcm
+
+
+class TestVRGenerators(unittest.TestCase):
+    """ Test all VR Generators
+    """
+
+    def test_generate_personal_name(self):
+        """ Test generate_personal_name()
+        """
+        pn = fill_dcm.vr_generators.generate_personal_name()
+        splitted_pn = pn.split('^')
+        self.assertEqual(len(splitted_pn), 2)
+        self.assertGreater(len(splitted_pn[0]), 0)
+        self.assertGreater(len(splitted_pn[1]), 0)
+
+    def test_generate_date(self):
+        """ Test generate_date() 
+        """
+        date = fill_dcm.vr_generators.generate_date()
+        self.assertEqual(len(date), 8)
+        self.assertTrue(date.isdigit())
+        # Check years
+        self.assertLessEqual(int(date[0:4]), 2020)
+        # Check months
+        self.assertLessEqual(int(date[4:6]), 12)
+        # Check days
+        self.assertLessEqual(int(date[6:]), 31)
+
+    def test_generate_patient_id(self):
+        """ Test generate_patient_id()
+        """
+        patient_id = fill_dcm.vr_generators.generate_id()
+        self.assertGreater(len(patient_id), 0)
+
+    def test_generate_lo(self):
+        """ Test generate_lo() 
+        """
+        long_string = fill_dcm.vr_generators.generate_lo()
+        self.assertLessEqual(len(long_string), 64)
+        self.assertGreaterEqual(len(long_string), 1)
+
+    def test_generate_age_string(self):
+        """ Test generate_age_string()
+        """
+        age_string = fill_dcm.vr_generators.generate_age_string()
+        self.assertEqual(len(age_string), 4)
+        self.assertTrue(age_string[0:3].isdigit())
+        self.assertEqual(age_string[-1], "Y")
+
+    def test_generate_decimal_string(self):
+        """ Test generate_decimal_string()
+        """
+        decimal_string = fill_dcm.vr_generators.generate_decimal_string()
+        self.assertNotEqual(len(decimal_string), 0)
+        self.assertLessEqual(len(decimal_string), 16)
+        for c in list(decimal_string):
+            self.assertIn(c, string.digits)
+
+    def test_generate_date_time(self):
+        """ Test generate_date_time() 
+        """
+        date_time = fill_dcm.vr_generators.generate_date_time()
+        self.assertEqual(len(date_time), 14)
+        self.assertTrue(date_time.isdigit())
+        # Check years
+        self.assertLessEqual(int(date_time[0:4]), 2020)
+        # Check months
+        self.assertLessEqual(int(date_time[4:6]), 12)
+        # Check days
+        self.assertLessEqual(int(date_time[6:8]), 31)
+        # Check hours
+        self.assertLessEqual(int(date_time[8:10]), 23)
+        # Check minutes
+        self.assertLessEqual(int(date_time[10:12]), 59)
+        # Check seconds
+        self.assertLessEqual(int(date_time[12:]), 59)
+
+    def test_generate_integer_string(self):
+        """ Test generate_integer_string()
+        """
+        integer_string = fill_dcm.vr_generators.generate_integer_string()
+        integer = int(integer_string)
+        self.assertGreaterEqual(integer, -1*2**31)
+        self.assertLessEqual(integer, (2**31)-1)
+
+    def test_generate_long_text(self):
+        """ Test generate_long_text() 
+        """
+        long_text = fill_dcm.vr_generators.generate_long_text()
+        self.assertLessEqual(len(long_text), 1024)
+        self.assertGreaterEqual(len(long_text), 1)
+
+    def test_generate_short_string(self):
+        """ Test generate_short_string() 
+        """
+        short_string = fill_dcm.vr_generators.generate_short_string()
+        self.assertLessEqual(len(short_string), 16)
+        self.assertGreaterEqual(len(short_string), 1)
+
+    def test_generate_short_text(self):
+        """ Test generate_short_text() 
+        """
+        short_text = fill_dcm.vr_generators.generate_short_text()
+        self.assertLessEqual(len(short_text), 1024)
+        self.assertGreaterEqual(len(short_text), 1)
+
+    def test_generate_time(self):
+        """ Test generate_time() 
+        """
+        time = fill_dcm.vr_generators.generate_time()
+        self.assertEqual(len(time), 6)
+        self.assertTrue(time.isdigit())
+        # Check hours
+        self.assertLessEqual(int(time[0:2]), 23)
+        # Check minutes
+        self.assertLessEqual(int(time[2:4]), 59)
+        # Check seconds
+        self.assertLessEqual(int(time[4:]), 59)
+
+    def test_generate_unique_identifier(self):
+        """ Test generate_unique_identifier()
+        """
+        unique_identifier = fill_dcm.vr_generators.generate_unique_identifier()
+        self.assertLessEqual(len(unique_identifier), 64)
+        splitted_uid = unique_identifier.split('.')
+        self.assertEqual(len(splitted_uid), 4)
+        for part in splitted_uid:
+            int(part)  # each part is a number
+
+    def test_generate_unsigned_short(self):
+        """ Test generate_unsigned_short()
+        """
+        unsigned_short = fill_dcm.vr_generators.generate_unsigned_short()
+        self.assertGreaterEqual(unsigned_short, 0)
+        self.assertLess(unsigned_short, 65536)
